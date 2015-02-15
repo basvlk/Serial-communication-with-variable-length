9bc4c2bd5e38cd285b1979431e70d4a7eb888c01
basvlk <basvlk@yahoo.com>
2015-02-15 03:37:26 +0000

    V0.5
    Default Diagnostic off and  0 delay.
    Increased nr of presets to 20
    now more robust for faulty input: Mode bytes only changed if all the
    received data is of the expected length

--- Serial_communication_-_variable_lenght_/Serial_communication_-_variable_lenght_.ino
+++ Serial_communication_-_variable_lenght_/Serial_communication_-_variable_lenght_.ino
@@ -39,9 +39,9 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(nLEDs, PIN, NEO_GRB + NEO_KHZ800);
 // PROGRAM CONTROL and Diagnostic
 const int ArduinoLedPin =  13;      // the number of the Arduino LED pin - it's blinking helps seeing if the program runs
 int Diagnostic = 1;                // switches on all kinds of diagnostic feedback from various locations in the program
-int LooptimeDiag = 1;              // minimal feedback for checking efficiency: only feeds back looptime
+int LooptimeDiag = 0;              // minimal feedback for checking efficiency: only feeds back looptime
 int ArrayDiag = 1;                 // if switched on, prints all arrays every cycle
-unsigned long Slowdown = 1500;                  // Delay value (ms) added to each loop, only in 'Diagnostic' mode to allow inspecting the data coming back over serial
+unsigned long Slowdown = 0;                  // Delay value (ms) added to each loop, only in 'Diagnostic' mode to allow inspecting the data coming back over serial
 unsigned long msTable[8] = {
   0, 100, 200, 500, 1000, 1500, 2000, 5000};
 unsigned long previousMillis = 0;  // will store last time at the end of the previous loop
@@ -49,11 +49,13 @@ unsigned long currentMillis = 0;  // will store the current time
 int LoopIteration = 0;             // to track loop iterations
 int BytesInBuffer = 0;             // number of unread Bytes waiting in the serial buffer
 int DiscardedBytes = 0;            // number of Bytes discarded (read out of the serial buffer but not used) since the last start of a read operation. Indicates something is wrong
-unsigned long CommsTimeout = 1000;    // When the program is expecting X bytes to arrive, this is how long it will wait before discarding
+unsigned long CommsTimeout = 200;    // When the program is expecting X bytes to arrive, this is how long it will wait before discarding
 unsigned long WaitedForBytes = 0;      //variable to time how long we've been waiting for the required bytes to arrive. To use to break out of a loop when Bytes don't arrive
 
 // PROGRAM - required for the core functionality of the sketch
 byte Mode = 0;
+byte PrevContMode = 0; //Previous Continuous Mode and Once Mode are temporarily stored, 
+byte PrevOnceMode = 0; //If data turns out to be invalid, ContMode and OnceMode are restored to the previous version
 byte ContMode = 0;
 byte OnceMode = 0;
 byte DataLength = 0;
@@ -63,17 +65,49 @@ int colorByte = 0;
 byte STATEX[24] = {
   0, 10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 0, 10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0,  };
 byte STATE10[nLEDs*3]= {
-  10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 0, 10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 10 };
+  0, 48, 56, 43, 78, 0, 8, 74, 0, 0, 74, 25, 0, 85, 72, 0, 50, 86, 0, 11, 85, 31, 0, 83, };
 byte STATE11[nLEDs*3]= {
-  10, 0, 0, 0, 50, 10, 10, 10, 10, 0, 0, 0, 10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 10 };
+  105, 0, 71, 0, 3, 101, 0, 23, 67, 0, 153, 205, 0, 153, 205, 0, 23, 67, 0, 3, 101, 105, 0, 71, };
 byte STATE12[nLEDs*3]= {
-  10, 0, 0, 0, 10, 10, 10, 50, 10, 0, 0, 0, 10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 10 };
+  86, 29, 0, 138, 120, 0, 0, 43, 8, 0, 99, 34, 35, 91, 0, 47, 59, 0, 48, 75, 0, 28, 13, 0, };
 byte STATE13[nLEDs*3]= {
-  10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 0, 50, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 10 };
+  92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102,  };
 byte STATE14[nLEDs*3]= {
-  10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 0, 10, 0, 0, 0, 10, 10, 10, 50, 10, 0, 0, 10 };
-  byte STATE15[nLEDs*3]= {
-  10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 0, 10, 0, 0, 0, 10, 10, 10, 50, 10, 0, 0, 10 }; 
+  23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, };
+byte STATE15[nLEDs*3]= {
+  96, 0, 2, 101, 0, 42, 80, 0, 105, 0, 3, 94, 0, 80, 91, 0, 86, 25, 39, 96, 0, 86, 87, 0, }; 
+  byte STATE16[24] = {
+  0, 10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 0, 10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0,  };
+byte STATE17[nLEDs*3]= {
+  0, 48, 56, 43, 78, 0, 8, 74, 0, 0, 74, 25, 0, 85, 72, 0, 50, 86, 0, 11, 85, 31, 0, 83, };
+byte STATE18[nLEDs*3]= {
+  105, 0, 71, 0, 3, 101, 0, 23, 67, 0, 153, 205, 0, 153, 205, 0, 23, 67, 0, 3, 101, 105, 0, 71, };
+byte STATE19[nLEDs*3]= {
+  86, 29, 0, 138, 120, 0, 0, 43, 8, 0, 99, 34, 35, 91, 0, 47, 59, 0, 48, 75, 0, 28, 13, 0, };
+byte STATE20[nLEDs*3]= {
+  0, 48, 56, 43, 78, 0, 8, 74, 0, 0, 74, 25, 0, 85, 72, 0, 50, 86, 0, 11, 85, 31, 0, 83, };
+byte STATE21[nLEDs*3]= {
+  105, 0, 71, 0, 3, 101, 0, 23, 67, 0, 153, 205, 0, 153, 205, 0, 23, 67, 0, 3, 101, 105, 0, 71, };
+byte STATE22[nLEDs*3]= {
+  86, 29, 0, 138, 120, 0, 0, 43, 8, 0, 99, 34, 35, 91, 0, 47, 59, 0, 48, 75, 0, 28, 13, 0, };
+byte STATE23[nLEDs*3]= {
+  92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102,  };
+byte STATE24[nLEDs*3]= {
+  23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, };
+byte STATE25[nLEDs*3]= {
+  96, 0, 2, 101, 0, 42, 80, 0, 105, 0, 3, 94, 0, 80, 91, 0, 86, 25, 39, 96, 0, 86, 87, 0, }; 
+  byte STATE26[24] = {
+  0, 10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 0, 10, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0,  };
+byte STATE27[nLEDs*3]= {
+  0, 48, 56, 43, 78, 0, 8, 74, 0, 0, 74, 25, 0, 85, 72, 0, 50, 86, 0, 11, 85, 31, 0, 83, };
+byte STATE28[nLEDs*3]= {
+  105, 0, 71, 0, 3, 101, 0, 23, 67, 0, 153, 205, 0, 153, 205, 0, 23, 67, 0, 3, 101, 105, 0, 71, };
+byte STATE29[nLEDs*3]= {
+  86, 29, 0, 138, 120, 0, 0, 43, 8, 0, 99, 34, 35, 91, 0, 47, 59, 0, 48, 75, 0, 28, 13, 0, };
+byte STATE30[nLEDs*3]= {
+  92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102, 92, 117, 0, 23, 0, 102,  };
+
+ 
 
 void setup() {
   Serial.begin(9600); 
@@ -118,10 +152,14 @@ void loop()
       //*************        R E A D       **********************
 
     { // SECTION 1: MODE and LENGTH
-      Serial.println("255! Yep");
+      Serial.println("Entered reading section: >=3 Bytes in buffer, first is 255");
+      PrevContMode = ContMode; //before reading in the mode, backing up the previous modes
+      PrevOnceMode = OnceMode;
+
       Mode=Serial.read();
       DataLength=Serial.read();
       BytesInBuffer = Serial.available();
+
       if (Mode > 99) { 
         ContMode = Mode; 
       }
@@ -131,9 +169,9 @@ void loop()
       if (Diagnostic == 1) { 
         Serial.print("[ Mode: ");
         Serial.print(Mode);
-        Serial.print(" // ContMode: ");
+        Serial.print(" // New ContMode: ");
         Serial.print(ContMode);
-        Serial.print(" // OnceMode: ");
+        Serial.print(" // New OnceMode: ");
         Serial.println(OnceMode);
         Serial.print("[ DataLength: ");
         Serial.print(DataLength);
@@ -143,6 +181,9 @@ void loop()
 
       //NOT ENOUGH BYTES
       if (BytesInBuffer < DataLength){
+        if (Diagnostic == 1) { 
+          Serial.println("[ Entering 'NOT ENOUGH BYTES");
+        }
         WaitedForBytes= 0;
         unsigned long StartMillis = millis();
 
@@ -162,10 +203,11 @@ void loop()
         }
         /// End of while loop. Now there are 2 options: either the bytes arrived, or they didn't and the thing timed out
         if (BytesInBuffer == DataLength){
-            if (Diagnostic == 1) {
-          Serial.print("[ Bytes arrived after waiting for  ");
-          Serial.print(WaitedForBytes);
-          Serial.println("ms");}
+          if (Diagnostic == 1) {
+            Serial.print("[ Bytes arrived after waiting for  ");
+            Serial.print(WaitedForBytes);
+            Serial.println("ms");
+          }
         }
 
         if (BytesInBuffer < DataLength){
@@ -176,16 +218,24 @@ void loop()
           Serial.println("ms, Aborting read operation, dumping data");
           char dump[BytesInBuffer];
           Serial.readBytes(dump, BytesInBuffer);
+          ContMode = PrevContMode; //restoring the previous modes so operation continues unchanged despite the invalid data
+          OnceMode = PrevOnceMode ;
         }
 
-      }
+      } // End of 'not enough Bytes'
+      
       //TOO MANY BYTES
       if (BytesInBuffer > DataLength) {
+        if (Diagnostic == 1) { 
+          Serial.println("[ Entering 'TOO MANY BYTES");
+        }
         Serial.print("[ ERROR: ");
         Serial.print(BytesInBuffer - DataLength);
         Serial.println(" too many Bytes in buffer. Dumping data ");
         char dump[BytesInBuffer];
         Serial.readBytes(dump, BytesInBuffer);
+        ContMode = PrevContMode; //restoring the previous modes so operation continues unchanged despite the invalid data
+        OnceMode = PrevOnceMode ;
       }
 
     }
@@ -198,10 +248,22 @@ void loop()
   } // End reading / discarding data section which only runs when there are 3 Bytes or ore in the buffer
 
 
+
+  ////// BRIDGE section, between validating the incoming data, and reading in the data (if valid)
+  if (Diagnostic == 1) { 
+    Serial.print("[ Last read Mode: ");
+    Serial.print(Mode);
+    Serial.print(" // Current ContMode: ");
+    Serial.print(ContMode);
+    Serial.print(" // Current OnceMode: ");
+    Serial.println(OnceMode);
+  }
+
+
   //*************       O N C E  M O D E S      **********************
   // this section represents modes that run once only - a state change, or a download of settings
 
-    switch (OnceMode) {
+  switch (OnceMode) {
   case 1: //All Off
     {
       for(int i=0; i<strip.numPixels(); i++) {
@@ -260,37 +322,37 @@ void loop()
       OnceMode = 0;
       break;
     }
-    case 20:
+  case 20:
     {
       ArrayToPixels(STATE10, 24);
       OnceMode = 0;
       break;
     }
-        case 21:
+  case 21:
     {
       ArrayToPixels(STATE11, 24);
       OnceMode = 0;
       break;
     }
-        case 22:
+  case 22:
     {
       ArrayToPixels(STATE12, 24);
       OnceMode = 0;
       break;
     }
-        case 23:
+  case 23:
     {
       ArrayToPixels(STATE13, 24);
       OnceMode = 0;
       break;
     }
-        case 24:
+  case 24:
     {
       ArrayToPixels(STATE14, 24);
       OnceMode = 0;
       break;
     }
-        case 25:
+  case 25:
     {
       ArrayToPixels(STATE15, 24);
       OnceMode = 0;
@@ -315,7 +377,7 @@ void loop()
       OnceMode = 0;
       break;
     }
-    
+
   default: 
     if (Diagnostic == 1) { 
       Serial.print("[ Once Mode ");
@@ -415,6 +477,9 @@ void LoopBlink(int Loop)
   }
 }
 // End blinkLed
+
+
+
 
 
 
